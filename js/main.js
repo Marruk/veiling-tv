1849101021976eafa9d4e31c80d0aed9fd4adb83
Mark Montvai <mark.montvai@gmail.com>
2025-05-05 00:22:21 +0200

    we hebben zo heerlijk samen gedebugged hihihihi

--- js/main.js
+++ js/main.js
@@ -94,7 +94,7 @@ const playJingle = async () => {
 }
 
 const randomized = a => {
-  const array = a.splice(0);
+  const array = [...a];
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
