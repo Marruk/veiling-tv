41c82d6a913a650f871510cbc921ac1e14b33bd9
Mark Montvai <mark.montvai@gmail.com>
2025-06-25 22:07:09 +0200

    add age, better undefined handling

--- js/main.js
+++ js/main.js
@@ -63,6 +63,10 @@ const show = () => {
         `;
       } else {
         const maxSpecialtyPoints = Math.max(...Object.values(rider.specialtyPoints));
+        const age = (_ => { 
+          const difference = new Date(Date.now() - new Date(rider.birthdate).getTime());
+          return Math.abs(difference.getUTCFullYear() - 1970);
+        })();
 
         if (Math.random() < 0.1) {
           rider.imageUrl = undefined;
@@ -77,9 +81,10 @@ const show = () => {
                 <img class="rider-flag" src="https://raw.githubusercontent.com/lipis/flag-icons/refs/heads/main/flags/4x3/${rider.nationality.toLowerCase()}.svg" />
               </div>
               <div class="rider-subtitle">
-                <span class="rider-weight">${rider.weight} kg</span> •
-                <span class="rider-height">${rider.height * 100} cm</span> •
-                <span class="rider-birthplace">Geboren in het ${['pittoreske', 'prachtige', 'fantastische', 'mooie', 'idyllische'][Math.round(Math.random() * 4)]} ${rider.placeOfBirth}</span>
+                <span class="rider-weight">${rider.weight ?? '?'} kg</span> •
+                <span class="rider-height">${rider.height ?? '?'} m</span> •
+                <span class="rider-age">${isNaN(age) ? '?' : age} jr</span> •
+                <span class="rider-birthplace">Geboren in het ${['pittoreske', 'prachtige', 'fantastische', 'mooie', 'idyllische'][Math.round(Math.random() * 4)]} ${rider.placeOfBirth ?? 'Weeknie'}</span>
               </div>
               <a class="rider-link" target="_blank" href="https://www.procyclingstats.com/${rider.url}">
                 <img src="./assets/pcs-logo.png">
